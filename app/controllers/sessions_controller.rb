fe2e80a0814b323ca7c0d3102f8ad33eeac36597
Saquib Hasnain <saquib.hasnain@kellogg.northwestern.edu>
2024-03-10 17:19:19 +0000

    LoginTrial_1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #Trying to find the user by their unqiue identifier
+    @user = User.find_by({"email" => params["email"]})
+    #If they exsit, then checking if they know their password correctly
+    if @user !=nil
+      #Then we checking their password matched with the stored password -> Login successfully
+      if @user["password"] == params["password"]
+        flash["notice"] ="Welcome."
+        redirect_to "/places"      
+      else
+        flash["notice"] ="Try Again."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] ="Try Again."
+      redirect_to "/login"
+    end
+
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources "entries"
   resources "places"
   resources "sessions"
+
   resources "users"
+    get("/login", :controller => "sessions", :action => "new")
+    get("/logout", :controller => "sessions", :action => "destroy")
 end
+
